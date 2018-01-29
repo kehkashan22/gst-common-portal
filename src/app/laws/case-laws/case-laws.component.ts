1c5421394bceb5b180482663acb778b479b38554
kehkashanF <myckehkashan@gmail.com>
2018-01-29 17:09:15 +0530

    chapters and section wise division

---
+++ src/app/laws/case-laws/case-laws.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-case-laws',
+  templateUrl: './case-laws.component.html',
+  styleUrls: ['./case-laws.component.css']
+})
+export class CaseLawsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
