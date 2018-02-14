a206fbbc989d61d2d8e5b2040415638745307700
zebsadique <zebsadique@gmail.com>
2018-02-14 12:34:15 +0530

    author page

--- src/app/author-profile/author-profile.component.ts
+++ src/app/author-profile/author-profile.component.ts
@@ -1,8 +1,10 @@
+import { StarsService } from 'app/shared/stars.service';
 import { AuthorsService } from './authors.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-author-profile',
@@ -14,13 +16,15 @@ export class AuthorProfileComponent implements OnInit {
   sub: any;
   author: any;
   articles = [];
-
+  authorStars: Observable<any>;
+  authorAvg: Observable<any>;
   constructor(private route: ActivatedRoute,
-  private _authors: AuthorsService) { }
+  private _authors: AuthorsService,
+private _stars: StarsService) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      this.author_id = params['a_id'];
+      this.author_id = params['a_id'] || '';
       this._authors.getAuthor(this.author_id).then((snap: firebase.firestore.DocumentSnapshot) => {
         this.author = {
           id: snap.id,
@@ -29,6 +33,9 @@ export class AuthorProfileComponent implements OnInit {
         console.log(this.author);
       });
       this.articles = this._authors.getArticleListByAuthor(this.author_id);
+    
+      this.authorStars = this._stars.getAuthorStars(this.author_id);
+      this.authorAvg = this._stars.getAvgRating(this.authorStars);
     });
   }
 
